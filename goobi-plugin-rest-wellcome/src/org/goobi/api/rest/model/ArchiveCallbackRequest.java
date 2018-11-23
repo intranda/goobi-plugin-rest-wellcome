c417bbd939e356e8467d0cb0da8d37f6ff1a5dd2
Oliver Paetzel <oliver.paetzel@intranda.com>
2018-11-23 11:48:35 +0100

    add jackson annotation to disregard unknown fields

--- goobi-plugin-rest-wellcome/src/org/goobi/api/rest/model/ArchiveCallbackRequest.java
+++ goobi-plugin-rest-wellcome/src/org/goobi/api/rest/model/ArchiveCallbackRequest.java
@@ -3,9 +3,12 @@ package org.goobi.api.rest.model;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ArchiveCallbackRequest {
     private String id;
     private String type;
